74df595633a1ba7a6c1607362ecfc5da7c9bdbc0
RobotLeopard86 <owensiebers@icloud.com>
2023-09-07 20:52:43 -0700

    Website update #1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to the RobotLeopard86 website!</h1>
+<h3>Pardon our mess, something big is coming!</h3>
